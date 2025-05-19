57667c387226683b4f7ad32de1f4db4cb579e980
Kozlov Leonid <jimjpoggers@yandex.ru>
2025-05-20 03:36:35 +0700

    Fix permissions

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -39,10 +39,10 @@ class UserViewSet(viewsets.ModelViewSet):
     pagination_class = PageLimitPagination
     http_method_names = ['get', 'post', 'put', 'delete']
 
-    def get_permissions(self):
-        if self.request.method == 'GET':
-            return (IsAuthenticatedOrReadOnly,)
-        return (IsAuthenticated,)
+    # def get_permissions(self):
+    #     if self.request.method == 'GET':
+    #         return (IsAuthenticatedOrReadOnly(),)
+    #     return (IsAuthenticated(),)
 
     def get_serializer_class(self):
         if self.request.method == 'POST':
@@ -69,6 +69,7 @@ class UserViewSet(viewsets.ModelViewSet):
         detail=True,
         methods=['post', 'delete'],
         url_path='subscribe',
+        # permission_classes=(IsAuthenticated,)
     )
     def subscribe(self, request, id=None):
         user = get_object_or_404(
@@ -113,6 +114,7 @@ class UserViewSet(viewsets.ModelViewSet):
         detail=False,
         methods=['get'],
         url_path='subscriptions',
+        # permission_classes=(IsAuthenticated,)
     )
     def subscriptions(self, request):
         queryset = self.get_recipes_annotated_queryset(
@@ -140,6 +142,7 @@ class UserViewSet(viewsets.ModelViewSet):
         detail=False,
         methods=['get'],
         url_path='me',
+        # permission_classes=(IsAuthenticated,)
     )
     def me(self, request):
         serializer = self.get_serializer(request.user)
@@ -152,6 +155,7 @@ class UserViewSet(viewsets.ModelViewSet):
         detail=False,
         methods=['put', 'delete'],
         url_path='me/avatar',
+        # permission_classes=(IsAuthenticated,)
     )
     def avatar(self, request):
         user = request.user
@@ -180,6 +184,7 @@ class UserViewSet(viewsets.ModelViewSet):
         detail=False,
         methods=['post'],
         url_path='set_password',
+        # permission_classes=(IsAuthenticated,)
     )
     def set_password(self, request):
         user = request.user
