833515826352e57109b91f0fa264ceefa7d03e50
Kozlov Leonid <jimjpoggers@yandex.ru>
2025-05-22 19:20:12 +0700

    fix flake8

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -6,9 +6,10 @@ from django.core.validators import MinValueValidator, RegexValidator
 from django.db import models
 from recipes.constants import (AVATAR_IMAGE_FOLDER, EMAIL_MAX_LENGTH,
                                MEASUREMENT_UNIT_MAX_LENGTH, MIN_COOKING_TIME,
-                               NAME_MAX_LENGTH, NAME_STR_WIDTH,
-                               RECIPE_IMAGE_FOLDER, SHORT_LINK_MAX_LENGTH,
-                               TAG_SLUG_MAX_LENGTH, USERNAME_STR_WIDTH, MIN_INGREDIENT_AMOUNT)
+                               MIN_INGREDIENT_AMOUNT, NAME_MAX_LENGTH,
+                               NAME_STR_WIDTH, RECIPE_IMAGE_FOLDER,
+                               SHORT_LINK_MAX_LENGTH, TAG_SLUG_MAX_LENGTH,
+                               USERNAME_STR_WIDTH)
 
 
 class User(AbstractUser):
