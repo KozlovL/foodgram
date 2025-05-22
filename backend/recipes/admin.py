4989377730195e888207ef5d85fe63b5152fdb97
Kozlov Leonid <jimjpoggers@yandex.ru>
2025-05-22 14:05:39 +0700

    Fix admin.py

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from recipes.models import (Favorite, Ingredient, Recipe, ShortLink, Subscribe,
-                            Tag, User, IngredientRecipe)
+from recipes.models import (Favorite, Ingredient, IngredientRecipe, Recipe,
+                            ShortLink, Subscribe, Tag, User)
 
 
 @admin.register(User)
