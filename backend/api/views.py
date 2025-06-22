6d5f8cc485452f598375745992ec06e2fed95660
Kozlov Leonid <jimjpoggers@yandex.ru>
2025-06-22 18:59:45 +0700

    fix download shopping cart

--- backend/api/views.py
+++ backend/api/views.py
@@ -366,7 +366,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def download_shopping_cart(self, request):
         ingredients = (
             IngredientRecipe.objects.filter(
-                recipe__shopping_cart__user=request.user,
+                recipe__in_shopping_cart__user=request.user,
             ).values(
                 'ingredient__name',
                 'ingredient__measurement_unit',
